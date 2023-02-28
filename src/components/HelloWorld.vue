89e1571ffc0b37d90c6d33afa14797fe04e6560a
nguyenvancongdev <congthang.nuce@gmail.com>
2023-02-28 20:47:03 +0700

    deploy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-helo anh em
-
+helo anh em {{ a }}
+<v-btn @click="conso(5)">ckick</v-btn>
 </template>
 
 <script>
@@ -9,75 +9,29 @@ export default {
   name: 'HelloWorld',
 
   data: () => ({
-    res: null,
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
+    a: 0,
+    nga: 0
+
   }),
   methods:{
-    conso(){
-      // cần phải test thật tốt cho giá trị ""
-      // tìm kiếm gía trị 
-      // let a = this.res?.hanh 
-      // let b = []
+   async conso(a){
       
-      // let c = (b?.hanh?.length )?  b?.hanh[4].cong[5] : "" 
-      // console.log(a)
-      // console.log('b la', b)
-      // console.log('c la', c)
-// tiep truc
-// ko the true false voi 
-// ""
+       let b = await new Promise( (res, rej)=> {
+        if (a>5) { res(1)}
+       else rej(6)
+       })
+        let c = b
+       let e =  b
+       let f = e
+       console.log('day la promí tranfe ne',f)
+      //  console.log(this.a)
+      console.log('day la promis', c)
+      
+    
 
-      let a = "0"
-      if (a){
-        console.log('1000', a)
-      }
+
      
-      //this.res = null thì khi chấm sẽ bị lỗi 
+     
     }
 
   },
