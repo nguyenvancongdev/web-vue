aa8a57858b567b3fb2bcfc8712e26196d996d8de
nguyenvancongdev <congthang.nuce@gmail.com>
2023-02-28 21:59:34 +0700

    deploy

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import CKEditor from '@ckeditor/ckeditor5-vue';
 import Vuex from 'vuex'
 
 loadFonts()
@@ -11,4 +12,5 @@ createApp(App)
   .use(router)
   .use(vuetify)
   .use(Vuex)
+  .use(CKEditor)
   .mount('#app')
