1948b2c84477c148c81601ff4998ff3c15a32c5e
nguyenvancongdev <72190917+nguyenvancongdev@users.noreply.github.com>
2023-03-01 09:22:13 +0700

    Create PostesBlog.vue

---
+++ src/components/user/PostesBlog.vue
@@ -0,0 +1,28 @@
+<template>
+
+      <div> đây là danh sách bài viết</div>
+
+
+   </template>
+   
+   <script>
+   export default {
+     name: 'PostesBlog',
+     data() {
+       return {
+         count: 0
+       }
+     },
+     methods:{
+       setTralet(){
+      
+       }
+     },
+     create(){
+     },
+   
+   }
+   </script>
+   
+ 
+ 
