c9875941952215b1b7036f0a858f9f0dd9f35b2e
nguyenvancongdev <congthang.nuce@gmail.com>
2024-01-15 20:16:01 +0700

    ff

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,19 +1,28 @@
 <template>
-    <div>
-      <menu-user />
-     </div>
-    <router-view></router-view>
+     <a-layout>
+      <a-layout-header :style="headerStyle">Header</a-layout-header>
+      <a-layout>
+      <a-layout-sider>Sider</a-layout-sider>
+      <a-layout-content>Content</a-layout-content>
+    </a-layout>
+      <a-layout-footer :style="footerStyle">Footer</a-layout-footer>
+    </a-layout>
  </template>
  
  <script>
  import { defineComponent } from 'vue';
- import MenuUser from '@/components/user/MenuUser';
+//  import MenuUser from '@/components/user/MenuUser';
  
  export default defineComponent({
    name: 'UserHello',
    components: {
-     MenuUser,
+    //  MenuUser,
    },
  });
  </script>
+<style scoped>
+.headerStyle {
+  
+}
+</style>
  
