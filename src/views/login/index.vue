9a04212fe7f8a8386f5386f8ec1114c7c806a1d5
congnv <congnv@etc.vn>
2024-01-24 16:20:31 +0700

    rr

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,42 @@
 <template>
-    <Form>
-    <Form.Item label="Username">
-      <Input v-model="username" />
-    </Form.Item>
-    <Form.Item label="Password">
-      <Input type="password" v-model="password" />
-    </Form.Item>
-    <Form.Item>
-      <Button type="primary" @click="login">Login</Button>
-    </Form.Item>
-  </Form>
-  <div></div>
+   <a-form
+    :model="formState"
+    name="basic"
+    :label-col="{ span: 8 }"
+    :wrapper-col="{ span: 16 }"
+    autocomplete="off"
+    @finish="onFinish"
+    @finishFailed="onFinishFailed"
+  >
+    <a-form-item
+      label="Username"
+      name="username"
+      :rules="[{ required: true, message: 'Please input your username!' }]"
+    >
+      <a-input v-model="formState.username" />
+    </a-form-item>
+
+    <a-form-item
+      label="Password"
+      name="password"
+      :rules="[{ required: true, message: 'Please input your password!' }]"
+    >
+      <a-input-password v-model="formState.password" />
+    </a-form-item>
+
+    <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
+      <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+    </a-form-item>
+
+    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>
+  </a-form>
+  <div @click="backtoHome">
+    backtoHome
+  </div>
  </template>
- 
+ <!-- https://antdv.com/components/form -->
  <script>
  import { defineComponent } from 'vue';
  
@@ -21,12 +45,13 @@
    data(){
     return {
       username: '',
-      password: ''
+      password: '',
+      formState: {},
     }
    },
    methods: {
     backtoHome(){
-        this.$router.push({name: 'home'})
+        this.$router.push("/")
     },
     backtologin(){
         this.$router.push({name: 'home'})
