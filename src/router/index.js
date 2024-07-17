9a422c59c53c2f6c392b6df4e84084954e45d1bd
Nguyen Văn Cong <congnv@etc.vn>
2024-07-17 13:57:09 +0700

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -20,5 +20,25 @@ const router = createRouter({
     }
   ]
 })
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const currentUser = auth.currentUser;
+
+  if (requiresAuth && !currentUser) {
+    next('/');
+  } else if (requiresAuth && currentUser) {
+    const userDoc = await db.collection('users').doc(currentUser.uid).get();
+    const userData = userDoc.data();
+    const role = to.meta.role;
+
+    if (role && userData.role !== role) {
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
