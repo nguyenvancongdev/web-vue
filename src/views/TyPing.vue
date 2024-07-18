af8ce15275af97575f052c46262b16fe03bc12c0
Nguyen Văn Cong <congnv@etc.vn>
2024-07-18 16:57:18 +0700

    deploy

--- src/views/TyPing.vue
+++ src/views/TyPing.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="about">
-     <a-textarea v-model:value="value" :rows="4" placeholder="maxlength is 6"  />
-     <a-textarea v-model:value="values" :rows="4" placeholder="maxlength is 6" />
+     <a-textarea v-model:value="value" :rows="4" placeholder="nhập text"  />
+     <div>
+     <a-textarea v-model:value="values" :rows="4" placeholder="" />
+      <button @click="copyValue">Copy Value</button>
+     </div>
+
 
   </div>
 </template>
@@ -16,154 +20,54 @@
 }
 </style>
 <script setup>
+ const telexMap = {
+        'awf': 'ằ', 'aws': 'ấ', 'awr': 'ẳ', 'awj': 'ặ', 'awx': 'ẵ', 'aw': 'ă',
+        'aaf': 'ầ', 'aas': 'ấ', 'aar': 'ẩ', 'aaj': 'ậ', 'aax': 'ẫ',  'aa': 'â',
+        'as': 'á', 'af': 'à', 'ax': 'ã', 'ar': 'ả', 'aj': 'ạ',
+         'eef': 'ề', 'ees': 'ế', 'eer': 'ể', 'eej': 'ệ', 'eex': 'ễ','ee': 'ê',
+        'ef': 'è', 'es': 'é', 'er': 'ẻ', 'ej': 'ẹ','ex': 'ẽ',
+        'if': 'ì', 'is': 'í', 'ir': 'ỉ', 'ij': 'ị', 'ix':'ĩ',
+        'oof': 'ồ', 'oos': 'ố', 'oor': 'ổ', 'ooj': 'ộ', 'oox': 'ỗ', 'oo': 'ô',
+        'ow': 'ơ', 'owf': 'ờ', 'ows': 'ớ', 'owr': 'ở', 'owj': 'ợ', 'owx': 'ỡ',
+        'of': 'ò', 'os': 'ó', 'or': 'ỏ', 'oj': 'ọ', 'ox':'õ',
+         'uf': 'ù', 'us': 'ú', 'ur': 'ủ', 'uj': 'ụ', 'ux':'ũ',
+        'uw': 'ư', 'uwf': 'ừ', 'uws': 'ứ', 'uwr': 'ử', 'uwj': 'ự', 'uwx': 'ữ',
+        'yf': 'ỳ', 'ys': 'ý', 'yr': 'ỷ', 'yj': 'ỵ', 'yx':'ỹ',
+        'dd': 'đ'
+    };
+function telexToVietnamese(telex) {
+   let vietnamese = telex;
+    for (const [key, value] of Object.entries(telexMap)) {
+        const regex = new RegExp(key, 'g');
+        vietnamese = vietnamese.replace(regex, value);
+    }
+     return vietnamese;
+}    
 import { useCounterStore } from '@/stores/counter'
 import { ref, computed, watchEffect   } from 'vue'
 
-const value = ref('rrrr')
+const value = ref('')
 
-const values = computed(() => convertTelexToVietnamese(value.value))
+const values = computed(() => telexToVietnamese(value.value))
+ const copyValue = () => {
+            // Tạo một thẻ textarea ẩn để đặt giá trị vào
+            var tempTextArea = document.createElement("textarea");
+            tempTextArea.value = values.value;
+            document.body.appendChild(tempTextArea);
 
+            // Chọn toàn bộ văn bản trong thẻ textarea
+            tempTextArea.select();
+            tempTextArea.setSelectionRange(0, 99999); // Hỗ trợ cho các trình duyệt khác nhau
 
-const VN_TELEX: any = {
-  á: "as",
-  à: "af",
-  ả: "ar",
-  ã: "ax",
-  ạ: "aj",
-  ắ: "aws",
-  ằ: "awf",
-  ẳ: "awr",
-  ẵ: "awx",
-  ặ: "awj",
-  ă: "aw",
-  ấ: "aas",
-  ầ: "aaf",
-  ẩ: "aar",
-  ẫ: "aax",
-  ậ: "aaj",
-  â: "aa",
-  đ: "dd",
-  é: "es",
-  è: "ef",
-  ẻ: "er",
-  ẽ: "ex",
-  ẹ: "ej",
-  ế: "ees",
-  ề: "eef",
-  ể: "eer",
-  ễ: "eex",
-  ệ: "eej",
-  ê: "ee",
-  í: "is",
-  ì: "if",
-  ỉ: "ir",
-  ĩ: "ix",
-  ị: "ij",
-  ó: "os",
-  ò: "of",
-  ỏ: "or",
-  õ: "ox",
-  ọ: "oj",
-  ố: "oos",
-  ồ: "oof",
-  ổ: "oor",
-  ỗ: "oox",
-  ộ: "ooj",
-  ô: "oo",
-  ớ: "ows",
-  ờ: "owf",
-  ở: "owr",
-  ỡ: "owx",
-  ợ: "owj",
-  ơ: "ow",
-  ú: "us",
-  ù: "uf",
-  ủ: "ur",
-  ũ: "ux",
-  ụ: "uj",
-  ứ: "uws",
-  ừ: "uwf",
-  ử: "uwr",
-  ữ: "uwx",
-  ự: "uwj",
-  ư: "uw",
-  ý: "ys",
-  ỳ: "yf",
-  ỷ: "yr",
-  ỹ: "yx",
-  ỵ: "yj",
-};
-const VN_TELEX_REVER: any = {
-  as: 'á',
-  af: 'à',
-  ar: 'ả',
-  ax: ã,
-  aj: ạ,
-  aws: ắ,
-  awf: ằ, 
-  awr: ẳ,
-  awx: ẵ,
-  awj: ặ,
-  aw: ă,
-  aas: ấ,
-  aaf: ầ,
-  aar: ẩ,
-  aax: ẫ,
-  aaj: ậ,
-   aa:  â,
-  đ "dd":, đ,
-  é "es":, é,
-  è "ef",  è,
-  ẻ: "er", ẻ,
-  ẽ: "ex", ẽ,
-  ẹ: "ej", ẹ,
-  ế: "ees", ế,
-  ề: "eef", ề,
-  ể: "eer", ể,
-  ễ: "eex", ễ,
-  ệ: "eej", ệ,
-  ê: "ee", ê,
-  í: "is", í,
-  ì: "if", ì,
-  ỉ: "ir", ỉ,
-  ĩ: "ix", ĩ,
-  ị: "ij", ị,
-  ó: "os", ó,
-  ò: "of", ò,
-  ỏ: "or", ỏ,
-  õ: "ox", õ,
-  ọ: "oj", ọ,
-  ố: "oos", ố,
-  ồ: "oof", ồ,
-  ổ: "oor", ổ,
-  ỗ: "oox", ỗ
-  ộ: "ooj", ộ
-  ô: "oo", ô
-  ớ: "ows", ớ
-  ờ: "owf", ờ
-  ở: "owr", ở
-  ỡ: "owx", ỡ
-  ợ: "owj", ợ
-  ơ: "ow", ơ
-  ú: "us", ú
-  ù: "uf", ù
-  ủ: "ur", ủ
-  ũ: "ux", ũ
-  ụ: "uj", ụ
-  ứ: "uws", ứ
-  ừ: "uwf", ừ
-  ử: "uwr", ử
-  ữ: "uwx", ữ
-  ự: "uwj", ự
-  ư: "uw", ư
-  ý: "ys", ý
-  ỳ: "yf", ỳ
-  ỷ: "yr", ỷ
-  ỹ: "yx", ỹ
-  ỵ: "yj", ỵ
-};
+            // Thực hiện lệnh sao chép
+            document.execCommand("copy");
 
+            // Xóa thẻ textarea sau khi sao chép
+            document.body.removeChild(tempTextArea);
 
-
+            // Thông báo cho người dùng biết là đã sao chép thành công (tuỳ chọn)
+            alert("Đã sao chép thành công!");
+        }
 
 </script>
+  
