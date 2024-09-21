848abb558d5c23a67a17ed75cb47731c7b3dd324
nguyenvancongdev <congthang.nuce@gmail.com>
2024-09-21 21:24:48 +0700

    rr

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,4 @@
-import { createRouter, createWebHistory, createWebHashHistory  } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import { createRouter, createWebHashHistory  } from 'vue-router'
-
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -31,25 +28,5 @@ const router = createRouter({
     }
   ]
 })
-// router.beforeEach(async (to, from, next) => {
-//   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-//   const currentUser = auth.currentUser;
-
-//   if (requiresAuth && !currentUser) {
-//     next('/');
-//   } else if (requiresAuth && currentUser) {
-//     const userDoc = await db.collection('users').doc(currentUser.uid).get();
-//     const userData = userDoc.data();
-//     const role = to.meta.role;
-
-//     if (role && userData.role !== role) {
-//       next('/');
-//     } else {
-//       next();
-//     }
-//   } else {
-//     next();
-//   }
-// });
 
 export default router
